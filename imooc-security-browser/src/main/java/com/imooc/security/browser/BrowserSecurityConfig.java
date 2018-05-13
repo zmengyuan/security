5c4f7601e6e563d706dcc04745214d9b7b2114df
zmengyuan <zmengyuan@126.com>
2018-05-13 22:13:36 +0800

    '4-4'

--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.imooc.security.browser;
 
+import com.imooc.security.core.properties.SecurityProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +12,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /*
+    配置类
+     */
+    @Autowired
+    private SecurityProperties securityProperties;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -21,7 +28,7 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+/*
         http.formLogin() //使用表单登录
 //                http.httpBasic()  //使用springsecurity默认的登录
                 .and()
@@ -29,4 +36,33 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()  //任何请求
                 .authenticated();  //都需要身份认证
     }
+  */
+
+/*
+        http.formLogin() //使用表单登录,自定义登录表单访问路径，自定义登录html
+                .loginPage("/imooc-signIn.html")
+                .loginProcessingUrl("/authentication/form") //这个设置主要是因为，security默认的登录是/login,要让自己的表单路径走登录流程，要在这里配置
+                .and()
+                .authorizeRequests() //下面这些都是授权的配置
+                .antMatchers("/imooc-signIn.html").permitAll()  //当访问这个url时候，不需要身份认证，就可以访问
+                .anyRequest()  //任何请求
+                .authenticated()//都需要身份认证
+                .and()
+                .csrf().disable();  //关闭csrf的防护
+        }
+*/
+
+        http.formLogin() //使用表单登录
+                .loginPage("/authentication/require") //为了处理不同前端传来的，所以这里不写死页面，由controller去处理，这里去访问controller路径
+                .loginProcessingUrl("/authentication/form") //这个设置主要是因为，security默认的登录是/login,要让自己的表单路径走登录流程，要在这里配置
+                .and()
+                .authorizeRequests() //下面这些都是授权的配置
+                .antMatchers("/authentication/require",
+                            securityProperties.getBrowser().getLoginPage()) //配置用户自定义的登录页面也放行
+                            .permitAll()  //当访问这个url时候，不需要身份认证，就可以访问
+                .anyRequest()  //任何请求
+                .authenticated()//都需要身份认证
+                .and()
+                .csrf().disable();  //关闭csrf的防护
+}
 }
