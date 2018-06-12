c037ecbe7a0be968afc57ed2d9c36d8c6460594a
zmengyuan <zmengyuan@126.com>
2018-06-12 21:33:03 +0800

    '4-9'

--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -4,15 +4,23 @@ import com.imooc.security.browser.authentication.ImoocAuthenticationSuccessHandl
 import com.imooc.security.core.properties.SecurityProperties;
 import com.imooc.security.core.validate.code.ValidateCodeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -35,6 +43,21 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     private AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
     @Autowired
     private AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+
+
+    @Qualifier("dataSource")
+    private DataSource dataSource ;
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        tokenRepository.setCreateTableOnStartup(true);
+        return tokenRepository;
+    }
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -94,7 +117,7 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable();  //关闭csrf的防护*/
 
 //        4-7
-        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
+        /*ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
         validateCodeFilter.setAuthenticationFailureHandler(imoocAuthenticationFailureHandler);
         validateCodeFilter.setSecurityProperties(securityProperties);
         validateCodeFilter.afterPropertiesSet();
@@ -117,6 +140,39 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()  //任何请求
                 .authenticated()//都需要身份认证
                 .and()
+                .csrf().disable();  //关闭csrf的防护*/
+
+//        4-8
+        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
+        validateCodeFilter.setAuthenticationFailureHandler(imoocAuthenticationFailureHandler);
+        validateCodeFilter.setSecurityProperties(securityProperties);
+        validateCodeFilter.afterPropertiesSet();
+
+
+        http
+                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)//添加该过滤器在后面这个过滤器之前
+                .formLogin() //使用表单登录
+                .loginPage("/authentication/require") //为了处理不同前端传来的，所以这里不写死页面，由controller去处理，这里去访问controller路径
+                .loginProcessingUrl("/authentication/form") //这个设置主要是因为，security默认的登录是/login,要让自己的表单路径走登录流程，要在这里配置
+                .successHandler(imoocAuthenticationSuccessHandler)//登录成功处理器
+                .failureHandler(imoocAuthenticationFailureHandler)//登录失败处理器
+
+                .and()
+                .rememberMe()
+                    .tokenRepository(persistentTokenRepository())
+                    .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
+                    .userDetailsService(userDetailsService)
+
+                .and()
+                .authorizeRequests() //下面这些都是授权的配置
+                .antMatchers("/authentication/require",
+                        securityProperties.getBrowser().getLoginPage(),//配置用户自定义的登录页面也放行
+                        "/code/image" //图形验证码接口
+                )
+                .permitAll()  //当访问这个url时候，不需要身份认证，就可以访问
+                .anyRequest()  //任何请求
+                .authenticated()//都需要身份认证
+                .and()
                 .csrf().disable();  //关闭csrf的防护
-}
+    }
 }
--- imooc-security-core/src/main/java/com/imooc/security/core/properties/BrowserProperties.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/properties/BrowserProperties.java
@@ -2,9 +2,14 @@ package com.imooc.security.core.properties;
 
 public class BrowserProperties {
 
+    /**登陆页*/
     private String loginPage = "/imooc-signIn.html";
 
+    /**登录数据类型*/
     private LoginType loginType = LoginType.JSON;
+
+    /**记住我有效时间*/
+    private int rememberMeSeconds = 3600;
 
     public LoginType getLoginType() {
         return loginType;
@@ -20,5 +25,13 @@ public class BrowserProperties {
 
     public void setLoginPage(String loginPage) {
         this.loginPage = loginPage;
+    }
+
+    public int getRememberMeSeconds() {
+        return rememberMeSeconds;
+    }
+
+    public void setRememberMeSeconds(int rememberMeSeconds) {
+        this.rememberMeSeconds = rememberMeSeconds;
     }
 }
